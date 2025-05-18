29051532a8163079644c859334c82ab0b837c8f4
Jakob Stechow <jakob.stechow@pm.me>
2025-05-18 10:48:17 +0200

    fix: log file should be in ~/.cache/file-explorer-tui

--- src/logger.rs
+++ src/logger.rs
@@ -1,8 +1,13 @@
 pub mod logger {
     use flexi_logger::{FileSpec, LoggerHandle};
 
+    use crate::env::env::get_home_dir;
+
     pub fn setup_logger_handle() -> LoggerHandle {
-        let default_file_spec = FileSpec::default();
+        let home_dir = get_home_dir().expect("$HOME is set");
+        let log_file_directory = home_dir + "/.cache/file-explorer-tui/";
+
+        let default_file_spec = FileSpec::default().directory(log_file_directory);
 
         flexi_logger::Logger::try_with_str("info")
             .unwrap()
